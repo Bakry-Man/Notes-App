d8b82cd8b0278620f21206dd413389eda1f49537
Bakry-Man <107670654+Bakry-Man@users.noreply.github.com>
2024-01-19 12:27:13 +0200

    create TypeAdapter for note

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/views/edit_note_view.dart';
 import 'package:notesapp/views/home_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('keyedBox');
+  await Hive.open('indexedBox');
   runApp(const NotesApp());
 }
 
